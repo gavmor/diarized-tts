e3ed21b8787bc415480005427e344a40fe1c2cd2
Gavin Morgan <morgan.gavin@gmail.com>
2024-12-08 15:48:53 -0800

    it owkrs

--- voi.sh
+++ voi.sh
@@ -2,23 +2,24 @@
 
 # Usage:
 # <input.txt voi.sh
+#
+# The input format is:
+#
+#     en_US:Where is my car?
+#     zh_CN:我的车在哪儿？
 
-MODEL_PATH=/media/userface/Data\ Windows/piper-voices/zh/zh_CN/huayan/medium/zh_CN-huayan-medium.onnx
+MODEL_DIR=/media/userface/Data\ Windows/piper-voices
 
+# Truncate file
 </dev/null > output.mp3
 
-while IFS=: read language speaker text; do
+while IFS=: read language text; do
+  model_path="$(find "$MODEL_DIR" -type f -name "$language*medium*onnx" | tail -1)"
   echo $text \
-  | piper --model "$MODEL_PATH" --output-raw \
+  | piper --model "$model_path" --output-raw \
   | lame -r -s 22 -m m --bitwidth 16 -b 192 - \
   >> "output.mp3"
 done
-
-# Execute the commands with parameters
-# echo "$TEXT" \
-#  | piper --model "$MODEL_PATH" --output-raw \
-#  | lame -r -s 22 -m m --bitwidth 16 -b 192 - \
-#  >> "$OUTPUT_FILE"
 
 # Play the output file
 cvlc "output.mp3"
