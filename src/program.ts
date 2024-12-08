9241d64456f307d93dab2c0fbf80ec72e634ba16
Gavin Morgan <morgan.gavin@gmail.com>
2024-12-08 10:24:34 -0800

    add default command

--- src/program.ts
+++ src/program.ts
@@ -2,7 +2,14 @@ import { Command } from "commander";
 import { foo } from "./foo";
 
 export const program = new Command();
+
+// Define a default command
+const defaultAction = () => {
+    console.log("No specific subcommand was provided.");
+};
+
 program
+    .action(defaultAction) // Set the default action here
     .command('foo')
     .argument('bar', 'An argument')
     .option('-q, --qux', 'An option')
